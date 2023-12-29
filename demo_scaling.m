3bc3b827a7bdbab4cd6b033fe9b3b0d67d420881
Ludvig af Klinteberg <ludvig.af.klinteberg@mdu.se>
2023-12-29 10:15:00 +0100

    Remove some dev files

--- demo_scaling.m
+++ demo_scaling.m
@@ -3,11 +3,13 @@ clear; rng(1);
 clf
 
 tol = 1e-6;
-ns = 280;
+ns  = 280;
 N_list = [];
 t_list = [];
 for N=ceil(logspace(2, 5, 30))
+    % Set max level to satisfy ns points per box
     max_level = max(ceil(log(N/ns)/log(8)), 1)
+    points = rand(N, 3)-1/2;
     charges = rand(N, 1)-1/2;
     disp("* DMK")
     atic = tic();
@@ -19,7 +21,9 @@ for N=ceil(logspace(2, 5, 30))
     N_list(end+1) = N;
     t_list(end+1) = wtime;
 end
-loglog(N_list, t_list, '.-', displayname=num2str(ns))
+loglog(N_list, t_list, '.-', displayname='DMK')
 hold on
 legend
-loglog(N_list, N_list * t_list(end)/N_list(end), '--')
+loglog(N_list, N_list * t_list(end)/N_list(end), '--', displayname='O(N)')
+xlabel('N')
+ylabel('time [s]')
