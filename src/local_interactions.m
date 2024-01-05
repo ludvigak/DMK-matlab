7a852c48c8a5e2c6259a2f1988c1a1c1d6f03f9a
Ludvig af Klinteberg <ludvig.af.klinteberg@mdu.se>
2024-01-05 11:37:33 +0100

    Add parfor to local interactions

--- src/local_interactions.m
+++ src/local_interactions.m
@@ -1,6 +1,19 @@
 function u=local_interactions(tree, charges, sigma_0)
     u = zeros(tree.N, 1);
-    for leaf_idx=tree.nonempty_leafs()
+    leaf_list = tree.nonempty_leafs();
+    Nleafs = numel(leaf_list);
+    udata = cell(1, Nleafs);
+    % Run in parallell if there is a parpool started
+    p = gcp('nocreate');
+    if isempty(p)
+        % There is no parallel pool
+        poolsize = 0;
+    else
+        % There is a parallel pool of <p.NumWorkers> workers
+        poolsize = p.NumWorkers;
+    end
+    parfor (i=1:Nleafs, poolsize)
+        leaf_idx=leaf_list(i);
         l = tree.boxLevels(leaf_idx);
         sigma_l = sigma_0 / 2^l;
         idxs = tree.box_point_idxs(leaf_idx);
@@ -13,7 +26,14 @@ function u=local_interactions(tree, charges, sigma_0)
             coll_charges = charges(coll_idxs);
             ubox = ubox + laplace_reskernel(box_points, coll_points, coll_charges, sigma_l);
         end
-        u(idxs) = ubox;
+        % Store output data from this iteration
+        udata{i} = ubox;
+    end
+    % Copy output data (from threads) to output structure
+    for i=1:Nleafs
+        leaf_idx=leaf_list(i);
+        idxs = tree.box_point_idxs(leaf_idx);    
+        u(idxs) = udata{i};
     end
 end
 
