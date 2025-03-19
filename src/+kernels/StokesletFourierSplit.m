56d94ddaf07089164bd9f7206afd455c3da29b58
Ludvig af Klinteberg <ludvig.af.klinteberg@mdu.se>
2025-03-19 14:27:35 +0100

    Cheb alternative also in stokeslet

--- src/+kernels/StokesletFourierSplit.m
+++ src/+kernels/StokesletFourierSplit.m
@@ -39,17 +39,33 @@ classdef StokesletFourierSplit < kernels.StokesletBase
             gamma_hat = fourier_scaling(self, k.^2, level);
             Bmollhat = Bwin .* gamma_hat;
             % Fourier integrals for evaluating mollified biharmonic
-            [f, df, d2f] = radial_fourier_kernels_gen();
-            %Bmoll   = @(r) 4*pi*hk*sum(  f(k,r) .* Bmollhat, 1) / (2*pi)^3;
-            dBmoll  = @(r) 4*pi*hk*sum( df(k,r) .* Bmollhat, 1) / (2*pi)^3;
-            d2Bmoll = @(r) 4*pi*hk*sum(d2f(k,r) .* Bmollhat, 1) / (2*pi)^3;
-            %  Residual biharmonic, including corrections
-            %Bres   = @(r) r + alpha + beta*r.^2 - Bmoll(r);
-            dBres  = @(r) 1 + 2*beta*r - dBmoll(r);
-            d2Bres = @(r) 2*beta - d2Bmoll(r);
-            % Construct chebfuns of diagonal and offdiagonal
-            Rdiag_cheb = chebfun(@(r)  r .* d2Bres(r) + dBres(r), [0 rl]);
-            Roffd_cheb = chebfun(@(r) -r .* d2Bres(r) + dBres(r), [0 rl]);
+            if true
+                % See comment in StressletFourierSplit.m
+                [f, df, d2f] = radial_fourier_kernels_gen();
+                %Bmoll   = @(r) 4*pi*hk*sum(  f(k,r) .* Bmollhat, 1) / (2*pi)^3;
+                dBmoll  = @(r) 4*pi*hk*sum( df(k,r) .* Bmollhat, 1) / (2*pi)^3;
+                d2Bmoll = @(r) 4*pi*hk*sum(d2f(k,r) .* Bmollhat, 1) / (2*pi)^3;
+                %  Residual biharmonic, including corrections
+                %Bres   = @(r) r + alpha + beta*r.^2 - Bmoll(r);
+                dBres  = @(r) 1 + 2*beta*r - dBmoll(r);
+                d2Bres = @(r) 2*beta - d2Bmoll(r);
+                % Construct chebfuns of diagonal and offdiagonal
+                Rdiag_cheb = chebfun(@(r)  r .* d2Bres(r) + dBres(r), [0 rl]);
+                Roffd_cheb = chebfun(@(r) -r .* d2Bres(r) + dBres(r), [0 rl]);
+            else
+                f = @(k,r)  k./r   .*(sin(k.*r));
+                Bmoll = @(r) 4*pi*hk*sum(  f(k,r) .* Bmollhat, 1) / (2*pi)^3;
+                Bres = @(r) r + alpha + beta*r.^2 - Bmoll(r);
+                Bres_cheb = chebfun(Bres, [0 rl], 'turbo');
+                r_cheb = chebfun(@(r) r, [0 rl]);
+                dBres_cheb = diff(Bres_cheb);
+                d2Bres_cheb = diff(Bres_cheb, 2);
+                d3Bres_cheb = diff(Bres_cheb, 3);
+                Rdiag_cheb = r_cheb.^2 .* d3Bres_cheb;
+                Roffd_cheb = dBres_cheb - r_cheb .* d2Bres_cheb + 1/3*r_cheb.^2 .* d3Bres_cheb;
+                Rdiag_cheb = r_cheb .* d2Bres_cheb + dBres_cheb;
+                Roffd_cheb = -r_cheb .* d2Bres_cheb + dBres_cheb;
+            end
             % Self interaction
             yself = -(2*k.^4)/3; % Limit df(r)/r+d2f(r) as r->0;
             c_self = 4*pi*hk*sum( yself .* Bmollhat, 1) / (2*pi)^3 - 4*beta;
